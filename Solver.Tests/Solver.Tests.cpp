20f51c7870b39562482c5c029808286445c56721
karmanniyblohodav <artt.lopatin2015@yandex.ru>
2024-05-31 02:45:19 +0300

    Добавьте файлы проекта.

---
+++ Solver.Tests/Solver.Tests.cpp
@@ -0,0 +1,15 @@
+﻿#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SolverTests
+{
+	TEST_CLASS(SolverTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
